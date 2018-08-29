3abd3e04b107902e4168e4b294e0fbb58f513485
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-29 16:01:21 -0700

    extraFeaturesExercise
    exercises 1-3

--- extraFeaturesExercises/Class1.cs
+++ extraFeaturesExercises/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace extraFeaturesExercises
 {
     public class Class1
     {
+        const string fName = "Jacob";
+
+        Me newMe = new Me("Jacob", "Moullet");
+
+        
+
+        
+
     }
 }
---
+++ extraFeaturesExercises/Me.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extraFeaturesExercises
+{
+    public class Me
+    {
+        public Me(string first, string last) : this(29, first, last) { }
+
+
+        public int age { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public Guid Id {get;set;}
+
+        public Me(int years, string first, string last)
+        {
+            age = years;
+            first = fName;
+            last = lName;
+            var newId = Guid.NewGuid();
+        }
+
+    }
+}
